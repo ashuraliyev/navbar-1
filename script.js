a47e067b83ba3838d78a3201abef22d5442a787a
ashuraliyev <jasurbekashuraliyev21@gmail.com>
2022-07-23 16:08:33 +0500

    Add new navbar

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+     $('.menu-icon').click(function(){
+         $(' .nav-list').toggleClass("active");
+         $(' .menu-icon i').toggleClass("active");
+     });
+ });
